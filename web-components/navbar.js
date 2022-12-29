ffe745c4bdc3fa89f0977f3494fbfda49a8d279a
webskydev <pmbasen@gmail.com>
2022-12-29 21:00:31 +0100

    Add styles to navbar

--- web-components/navbar.js
+++ web-components/navbar.js
@@ -1,12 +1,32 @@
 const template = document.createElement('template');
 
 template.innerHTML = `
-  <navbar>
-    <ul>
-      <li>Blog</li>
-      <li>O mnie</li>
-    </ul>
-  </navbar>
+  <style>
+    a {
+      text-decoration: none;
+      color: var(--font-color-primary);
+    }
+    
+    a:hover {
+      color: var(--color-primary);
+    }
+
+    .navbar {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  </style>
+
+  <header class="navbar">
+    <a href="#">webskydev</a>
+    <nav>
+      <ul>
+        <li>Blog</li>
+        <li>O mnie</li>
+      </ul>
+    </nav>
+  </header>
 `
 
 export class BlogNavbar extends HTMLElement {
